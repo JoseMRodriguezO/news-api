b21c371dce0d83898a1f9ac619385d536eac5b24
JoseMRodriguez <Josemiguelrodriguez89@gmail.com>
2023-02-15 14:12:40 -0600

    add webbrower support

--- news-api.py
+++ news-api.py
@@ -3,8 +3,10 @@
 from pprint import pprint
 from newsapi import NewsApiClient
 import click
+import webbrowser
 URL = ('https://newsapi.org/v2/top-headlines?country=us')
 api = NewsApiClient(api_key="bdc4bcc577c34dc194527f8c27c71208")
+
 
 print("Welcome, let's see the news")
 category = input(
@@ -12,17 +14,21 @@ category = input(
 top_headlines = None
 
 valid_categories = ['business', 'entertainment',
-                    'general', 'health', 'science', 'sports', 'tecnology']
+                    'general', 'health', 'science', 'sports', 'technology']
 if category not in valid_categories:
     print('Invalid category. Please choose from the following categories: business, entertainment, general, health, science, sports, technology')
 else:
     top_headlines = api.get_top_headlines(category=category)
+
 if top_headlines is not None:
     for article in top_headlines['articles']:
-
-        for article in top_headlines['articles']:
-            title = (article['title'] + "\n")
-            url = article['url']
-            description = article['description']
-            click.echo(
-                f"{click.style(title, fg='green')}\n{click.style(description, fg='white')}\n{click.style(url, fg='blue', underline=True)}\n")
+        title = (article['title'] + "\n")
+        url = article['url']
+        description = article['description']
+        click.echo(
+            f"{click.style(title, fg='green')}\n{click.style(description, fg='white')}\n{click.style(url, fg='blue', underline=True)}\n")
+        open_url = input("Do you want to read the full article? (y/n): ")
+        if open_url.lower() == 'y':
+            webbrowser.open(url, new=2)
+        elif open_url.lower() == 'n':
+            continue
