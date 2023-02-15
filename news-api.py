e3e124758c7b4cee8f897439921275ac2f3b648a
JoseMRodriguez <Josemiguelrodriguez89@gmail.com>
2023-02-15 11:35:59 -0600

    add space between headlines and underline and color for url

--- news-api.py
+++ news-api.py
@@ -2,6 +2,7 @@
 
 from pprint import pprint
 from newsapi import NewsApiClient
+import click
 URL = ('https://newsapi.org/v2/top-headlines?country=us')
 api = NewsApiClient(api_key="bdc4bcc577c34dc194527f8c27c71208")
 
@@ -9,4 +10,9 @@ top_headlines = api.get_top_headlines()
 
 
 for article in top_headlines['articles']:
-    pprint(f"{article['title']} ({article['url']})")
+    pprint
+    for article in top_headlines['articles']:
+        title = (article['title'] + "\n")
+        url = article['url']
+        click.echo(
+            f"{title}\n{click.style  (url, fg='blue', underline=True)}\n")
