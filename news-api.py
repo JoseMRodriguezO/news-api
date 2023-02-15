bb1f1906c599077a8316df17a432441dd45d2487
JoseMRodriguez <Josemiguelrodriguez89@gmail.com>
2023-02-15 13:19:29 -0600

    create a category

--- news-api.py
+++ news-api.py
@@ -6,13 +6,22 @@ import click
 URL = ('https://newsapi.org/v2/top-headlines?country=us')
 api = NewsApiClient(api_key="bdc4bcc577c34dc194527f8c27c71208")
 
-top_headlines = api.get_top_headlines()
+print("Welcome, let's see the news")
+category = input(
+    "Select a news category (business, entertainment, general, health, science, sports, technology): ")
+top_headlines = None
 
-
-for article in top_headlines['articles']:
-    pprint
+valid_categories = ['business', 'entertainment',
+                    'general', 'health', 'science', 'sports', 'tecnology']
+if category not in valid_categories:
+    print('Invalid category. Please choose from the following categories: business, entertainment, general, health, science, sports, technology')
+else:
+    top_headlines = api.get_top_headlines(category=category)
+if top_headlines is not None:
     for article in top_headlines['articles']:
-        title = (article['title'] + "\n")
-        url = article['url']
-        click.echo(
-            f"{title}\n{click.style  (url, fg='blue', underline=True)}\n")
+        pprint
+        for article in top_headlines['articles']:
+            title = (article['title'] + "\n")
+            url = article['url']
+            click.echo(
+                f"{title}\n{click.style  (url, fg='blue', underline=True)}\n")
