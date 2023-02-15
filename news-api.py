1f4c19ffadd4958d4b73bb7275085795abf3f831
JoseMRodriguez <Josemiguelrodriguez89@gmail.com>
2023-02-15 11:08:46 -0600

    change news to topheadlines

--- news-api.py
+++ news-api.py
@@ -1,11 +1,12 @@
 
-from datetime import datetime
+
 from pprint import pprint
 from newsapi import NewsApiClient
-URL = ("https://newsapi.org/v2/everything?q=Nba&sortBy=popularity")
+URL = ('https://newsapi.org/v2/top-headlines?country=us')
 api = NewsApiClient(api_key="bdc4bcc577c34dc194527f8c27c71208")
 
-response = api.get_everything(q='nba', sources='reuters')
+top_headlines = api.get_top_headlines()
 
 
-pprint(response['articles'])
+for article in top_headlines['articles']:
+    pprint(f"{article['title']} ({article['url']})")
