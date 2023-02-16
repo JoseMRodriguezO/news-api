352ac3cce0e6acd3afbe18f2eba206645a0c7746
JoseMRodriguez <Josemiguelrodriguez89@gmail.com>
2023-02-15 18:01:29 -0600

    loop to show all the top-20 news

--- news-api.py
+++ news-api.py
@@ -9,26 +9,20 @@ api = NewsApiClient(api_key="bdc4bcc577c34dc194527f8c27c71208")
 
 
 print("Welcome, let's see the news")
-category = input(
-    "Select a news category (business, entertainment, general, health, science, sports, technology): ")
-top_headlines = None
 
-valid_categories = ['business', 'entertainment',
-                    'general', 'health', 'science', 'sports', 'technology']
-if category not in valid_categories:
-    print('Invalid category. Please choose from the following categories: business, entertainment, general, health, science, sports, technology')
-else:
-    top_headlines = api.get_top_headlines(category=category)
+category = 'general'
+top_headlines = api.get_top_headlines(category=category)
 
 if top_headlines is not None:
-    for article in top_headlines['articles']:
-        title = (article['title'] + "\n")
+    articles = top_headlines['articles']
+    for index, article in enumerate(articles):
+        title = (f"{index + 1}. {article['title']}")
+        click.echo(click.style(title, fg='green'))
+
+    index = int(input("Enter the number of the article you want to read: ")) - 1
+    if 0 <= index < len(articles):
+        article = articles[index]
         url = article['url']
-        description = article['description']
-        click.echo(
-            f"{click.style(title, fg='green')}\n{click.style(description, fg='white')}\n{click.style(url, fg='blue', underline=True)}\n")
-        open_url = input("Do you want to read the full article? (y/n): ")
-        if open_url.lower() == 'y':
-            webbrowser.open(url, new=2)
-        elif open_url.lower() == 'n':
-            continue
+        webbrowser.open(url, new=2)
+    else:
+        print("Invalid article number")
